0e681539b02a750cce3d4218ed15915399bd894c
unknown <devliyalhimanshu@gmsil.com>
2025-10-01 18:55:07 +0530

    hh

--- src/app/books/page.js
+++ src/app/books/page.js
@@ -1,7 +1,17 @@
 "use client";
 
+import { Breadcrumb } from "@/components/breadcrumb";
 import Books from "@/home/books";
 
 export default function ProductsSection() {
-  return <Books></Books>;
+  return (
+    <>
+      <Breadcrumb
+        title="Books"
+        backgroundImage="/img/header1.webp"
+        items={[{ label: "Books", href: "/books", isCurrent: true }]}
+      />
+      <Books></Books>
+    </>
+  );
 }
