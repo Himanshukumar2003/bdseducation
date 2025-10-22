82f7c979ab4eff9e8e7ae3b0038cef08cf499ea4
unknown <devliyalhimanshu@gmsil.com>
2025-10-22 13:47:25 +0530

    first commit

--- src/app/become-a-distributor/page.js
+++ src/app/become-a-distributor/page.js
@@ -24,82 +24,96 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Distributor from "./_components/form";
+import { Breadcrumb } from "@/components/breadcrumb";
 
 export default function PartnerSection() {
   return (
-    <section className="max-w-7xl mx-auto px-6 section">
-      <div className="text-center mb-16 max-w-[900px] mx-auto">
-        <h2 className="text-3xl lg:text-4xl font-bold text-gray-800 mb-6">
-          Partner with BDS Education – Bring Future-Ready Learning to Your
-          Region
-        </h2>
-      </div>
-      <div className="grid lg:grid-cols-12 gap-12 items-center">
-        <div className="col-span-6">
-          <p className="text-gray-600 mb-5 leading-relaxed">
-            We offer a complete A-to-Z solution for teaching Coding, Robotics,
-            and Future Technologies, designed to serve the unique needs of urban
-            and rural schools, colleges, and training institutes across India.
-            Our integrated ecosystem empowers educators and institutions with
-            everything they need to make learning hands-on, engaging, and
-            future-ready:
-          </p>
-
-          <ul>
-            <li>
-              <p className="group flex gap-2 px-6 py-3 border-2 border-blue-900 rounded-full font-medium w-auto mb-2 bg-transparent transition-all duration-300 ease-in-out hover:bg-blue-900 hover:text-white shadow-[0px_5px_0px_white] transform -translate-y-1 cursor-pointer">
-                <Check className="  group-hover:text-white  text-blue-900"></Check>{" "}
-                High-quality educational products and teaching kits
-              </p>
-            </li>
-            <li>
-              <p className="group flex gap-2 px-6 py-3 border-2 border-blue-900 rounded-full font-medium w-auto mb-2 bg-transparent transition-all duration-300 ease-in-out hover:bg-blue-900 hover:text-white  shadow-[0px_5px_0px_white] transform -translate-y-1 cursor-pointer">
-                <Check className="  group-hover:text-white  text-blue-900"></Check>{" "}
-                Structured, curriculum-aligned content
-              </p>
-            </li>
-            <li>
-              <p className="group flex gap-2 px-6 py-3 border-2 border-blue-900 rounded-full font-medium w-auto mb-2 hover:text-white bg-transparent transition-all duration-300 ease-in-out hover:bg-blue-900  shadow-[0px_5px_0px_white] transform -translate-y-1 cursor-pointer">
-                <Check className="  group-hover:text-white  text-blue-900"></Check>{" "}
-                Grade- and technology-specific books and Manuals
-              </p>
-            </li>
-            <li>
-              <p className="  group  flex gap-2 px-6 py-3 border-2 border-blue-900 rounded-full font-medium w-auto mb-2 hover:text-white bg-transparent transition-all duration-300 ease-in-out hover:bg-blue-900  shadow-[0px_5px_0px_white] transform -translate-y-1 cursor-pointer">
-                <Check className="  group-hover:text-white  text-blue-900"></Check>{" "}
-                Extensive code libraries and project resource Manuals
-              </p>
-            </li>
-            <li>
-              <p className="group flex gap-2 px-6 py-3 border-2 border-blue-900 rounded-full font-medium w-auto mb-2 bg-transparent hover:text-white transition-all duration-300 ease-in-out hover:bg-blue-900  shadow-[0px_5px_0px_white] transform -translate-y-1 cursor-pointer">
-                <Check className="  group-hover:text-white  text-blue-900"></Check>{" "}
-                Ongoing training and support for educators
-              </p>
-            </li>
-          </ul>
-
-          <p className="text-gray-600 mb-5 leading-relaxed">
-            With this proven framework, our partners help students develop
-            <b> 21st-century</b> skills while ensuring sustained engagement and
-            <b> measurable learning outcomes</b>.<br></br> If you have strong
-            local connections and are passionate about transforming education in
-            your community, we invite you to join hands with us. <br></br> Our
-            partnership model is designed to help you build a reliable,
-            long-term income stream while creating a network of inspired
-            learners and satisfied institutions.
-          </p>
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button className="btn">Become a Distributor</Button>
-            </DialogTrigger>
-            <DialogContent className="max-h-5xl">
-              <Distributor></Distributor>
-            </DialogContent>
-          </Dialog>
+    <>
+      <Breadcrumb
+        title="Become a Distributor"
+        backgroundImage="/img/header1.webp"
+        items={[
+          {
+            label: "become-a-distributor",
+            href: "/become-a-distributor",
+            isCurrent: true,
+          },
+        ]}
+      />
+      <section className="max-w-7xl mx-auto px-6 section">
+        <div className="text-center mb-16 max-w-[900px] mx-auto">
+          <h2 className="text-3xl lg:text-4xl font-bold text-gray-800 mb-6">
+            Partner with BDS Education – Bring Future-Ready Learning to Your
+            Region
+          </h2>
         </div>
+        <div className="grid lg:grid-cols-12 gap-12 items-center">
+          <div className="col-span-6">
+            <p className="text-gray-600 mb-5 leading-relaxed">
+              We offer a complete A-to-Z solution for teaching Coding, Robotics,
+              and Future Technologies, designed to serve the unique needs of
+              urban and rural schools, colleges, and training institutes across
+              India. Our integrated ecosystem empowers educators and
+              institutions with everything they need to make learning hands-on,
+              engaging, and future-ready:
+            </p>
 
-        {/* Right Section - Steps with Icons */}
-        {/* <div className="space-y-6">
+            <ul>
+              <li>
+                <p className="group flex gap-2 px-6 py-3 border-2 border-blue-900 rounded-full font-medium w-auto mb-2 bg-transparent transition-all duration-300 ease-in-out hover:bg-blue-900 hover:text-white shadow-[0px_5px_0px_white] transform -translate-y-1 cursor-pointer">
+                  <Check className="  group-hover:text-white  text-blue-900"></Check>{" "}
+                  High-quality educational products and teaching kits
+                </p>
+              </li>
+              <li>
+                <p className="group flex gap-2 px-6 py-3 border-2 border-blue-900 rounded-full font-medium w-auto mb-2 bg-transparent transition-all duration-300 ease-in-out hover:bg-blue-900 hover:text-white  shadow-[0px_5px_0px_white] transform -translate-y-1 cursor-pointer">
+                  <Check className="  group-hover:text-white  text-blue-900"></Check>{" "}
+                  Structured, curriculum-aligned content
+                </p>
+              </li>
+              <li>
+                <p className="group flex gap-2 px-6 py-3 border-2 border-blue-900 rounded-full font-medium w-auto mb-2 hover:text-white bg-transparent transition-all duration-300 ease-in-out hover:bg-blue-900  shadow-[0px_5px_0px_white] transform -translate-y-1 cursor-pointer">
+                  <Check className="  group-hover:text-white  text-blue-900"></Check>{" "}
+                  Grade- and technology-specific books and Manuals
+                </p>
+              </li>
+              <li>
+                <p className="  group  flex gap-2 px-6 py-3 border-2 border-blue-900 rounded-full font-medium w-auto mb-2 hover:text-white bg-transparent transition-all duration-300 ease-in-out hover:bg-blue-900  shadow-[0px_5px_0px_white] transform -translate-y-1 cursor-pointer">
+                  <Check className="  group-hover:text-white  text-blue-900"></Check>{" "}
+                  Extensive code libraries and project resource Manuals
+                </p>
+              </li>
+              <li>
+                <p className="group flex gap-2 px-6 py-3 border-2 border-blue-900 rounded-full font-medium w-auto mb-2 bg-transparent hover:text-white transition-all duration-300 ease-in-out hover:bg-blue-900  shadow-[0px_5px_0px_white] transform -translate-y-1 cursor-pointer">
+                  <Check className="  group-hover:text-white  text-blue-900"></Check>{" "}
+                  Ongoing training and support for educators
+                </p>
+              </li>
+            </ul>
+
+            <p className="text-gray-600 mb-5 leading-relaxed">
+              With this proven framework, our partners help students develop
+              <b> 21st-century</b> skills while ensuring sustained engagement
+              and
+              <b> measurable learning outcomes</b>.<br></br> If you have strong
+              local connections and are passionate about transforming education
+              in your community, we invite you to join hands with us. <br></br>{" "}
+              Our partnership model is designed to help you build a reliable,
+              long-term income stream while creating a network of inspired
+              learners and satisfied institutions.
+            </p>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button className="btn">Become a Distributor</Button>
+              </DialogTrigger>
+              <DialogContent className="max-h-5xl">
+                <Distributor></Distributor>
+              </DialogContent>
+            </Dialog>
+          </div>
+
+          {/* Right Section - Steps with Icons */}
+          {/* <div className="space-y-6">
         {steps.map((item) => (
           <Card
             key={item.id}
@@ -120,16 +134,17 @@ export default function PartnerSection() {
           </Card>
         ))}
       </div> */}
-        <div className="col-span-6 h-full">
-          <Image
-            src="/images/partner.jpeg"
-            width={500}
-            height={500}
-            alt=""
-            className="w-full h-full rounded-2xl"
-          />
+          <div className="col-span-6 h-full">
+            <Image
+              src="/images/partner.jpeg"
+              width={500}
+              height={500}
+              alt=""
+              className="w-full h-full rounded-2xl"
+            />
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
--- src/components/footer.js
+++ src/components/footer.js
@@ -45,9 +45,7 @@ export default function Footer() {
 
           {/* Company Section */}
           <div>
-            <h3 className="text-lg font-semibold text-gray-400 mb-4">
-              Company
-            </h3>
+            <h3 className="text-lg font-semibold text-white mb-4">Company</h3>
             <ul className="space-y-3">
               <li>
                 <Link
@@ -108,7 +106,7 @@ export default function Footer() {
           {/* Products / Solutions Section */}
 
           <div>
-            <h3 className="text-lg font-semibold text-gray-400 mb-4">
+            <h3 className="text-lg font-semibold text-white mb-4">
               Useful Links
             </h3>
             <ul className="space-y-3">
@@ -148,9 +146,7 @@ export default function Footer() {
           </div>
 
           <div className="lg:col-span-2">
-            <h3 className="text-lg font-semibold text-gray-400 mb-4">
-              Contact
-            </h3>
+            <h3 className="text-lg font-semibold text-white mb-4">Contact</h3>
             <div className="space-y-4">
               {/* Dayalbagh Address */}
               <div className="flex items-start gap-3">
